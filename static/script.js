d10b570d6ea97e33f9ead7ab3f0192ae8add0266
JareBear <jaredgrozz@gmail.com>
2024-06-20 18:45:14 -0500

    Fixed minor bugs

--- static/script.js
+++ static/script.js
@@ -8,14 +8,14 @@ M.FormSelect.init(selects);
 
 $(".home-button-collapse").sideNav();
 $(".home-button-collapse").sideNav({
-    menuWidth: 300, // Default is 300
+    menuWidth: 250, // Default is 300
     edge: "left", // Choose the horizontal origin
     closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
     draggable: true, // Choose whether you can drag to open on touch screens,
 });
 $(".song-button-collapse").sideNav();
 $(".song-button-collapse").sideNav({
-    menuWidth: 300, // Default is 300
+    menuWidth: 250, // Default is 300
     edge: "left", // Choose the horizontal origin
     closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
     draggable: true, // Choose whether you can drag to open on touch screens,
@@ -686,7 +686,7 @@ class SongPage {
             this.navbarArrangementName.innerHTML = song.categories;
             this.highlightCurrentSong(songName);
             this.searchSongs();
-            if (this.singAlong) {
+            if (this.singAlong && this.singAlong.isHost) {
                 this.singAlong.changeSong(songName);
             }
             $('.song-button-collapse').sideNav('hide');
